900720f0b6d4168bb80fc358f6de3e9ecaae7279
Hyojin Kwak <hjkwak91@gmail.com>
2018-10-28 02:40:11 +0900

    해커톤 안드로이드 모바일 최종
    - 코멘트 추가 버튼
    - assets 교체
    - url 정보 없을 때 다이얼로그 추가

--- app/src/main/assets/www/index.js
+++ app/src/main/assets/www/index.js
@@ -1,6 +1,15 @@
 
-function testFuncCall(address, contents) {
-  const message = `Address: ${address}, Contents: ${contents}`
+function addComment(address, contents, df, ddd, commendd) {
+    localStorage.setItem("address", "Hello World!");
+  const message = `Address: ${address}, Contents: ${contents}, ${df}, ${ddd}, ${commendd}`
   alert(message)
   return
+
 }
+
+function sendMessage(msg){
+		window.android.finishPosting(msg);
+		return
+}
+
+
